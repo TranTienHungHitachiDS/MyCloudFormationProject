4750f97aadcf28e903a2bf01c9a6c64a7b3bf96b
Tran Tien Hung <trantien.hung@hitachids.com>
2026-01-07 15:43:00 +0700

    feat: change project name and remove old-templates folder

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,15 @@ PROFILE="personal"
 
 
 # Create stacks in order
-sh network/create_network_stack.sh $PROFILE "mdms-network-stack"
-sh iam/create_iam_stack.sh $PROFILE "mdms-iam-stack"
-sh rds/create_rds_stack.sh $PROFILE "mdms-rds-stack"
-# sh dynamodb/create_dynamodb_stack.sh $PROFILE "mdms-dynamodb-stack"
-sh rds/create_rds_stack.sh $PROFILE "mdms-rds-stack"
-sh efs/create_efs_stack.sh $PROFILE "mdms-efs-stack"
-# sh lambda-functions/create_lambda_stack.sh $PROFILE "mdms-lambda-stack"
-sh load-balancer/create_lb_stack.sh $PROFILE "mdms-lb-stack"
-sh ecs-task-definitions/create_ecs_tf_stack.sh $PROFILE "mdms-ecs-tf-stack"
-sh ecs/create_ecs_stack.sh $PROFILE "mdms-ecs-stack"
+sh network/create_network_stack.sh $PROFILE "dkhp-network-stack"
+sh iam/create_iam_stack.sh $PROFILE "dkhp-iam-stack"
+sh rds/create_rds_stack.sh $PROFILE "dkhp-rds-stack"
+sh dynamodb/create_dynamodb_stack.sh $PROFILE "dkhp-dynamodb-stack"
+sh efs/create_efs_stack.sh $PROFILE "dkhp-efs-stack"
+sh lambda-functions/create_lambda_stack.sh $PROFILE "dkhp-lambda-stack"
+sh load-balancer/create_lb_stack.sh $PROFILE "dkhp-lb-stack"
+sh ecs-task-definitions/create_ecs_tf_stack.sh $PROFILE "dkhp-ecs-tf-stack"
+sh ecs/create_ecs_stack.sh $PROFILE "dkhp-ecs-stack"
 
 
 echo "Created all stack successfully"
