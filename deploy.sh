e24a04cb7499522d4f05e87df2be2ce98a8e5720
Tran Tien Hung <trantien.hung@hitachids.com>
2026-01-09 14:36:37 +0700

    feat: update destroy.sh and ecs-task-definitions stack

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ PROFILE="personal"
 
 
 # Create stacks in order
-sh network/create_network_stack.sh $PROFILE "dkhp-network-stack"
-sh iam/create_iam_stack.sh $PROFILE "dkhp-iam-stack"
-sh rds/create_rds_stack.sh $PROFILE "dkhp-rds-stack"
-sh dynamodb/create_dynamodb_stack.sh $PROFILE "dkhp-dynamodb-stack"
-sh efs/create_efs_stack.sh $PROFILE "dkhp-efs-stack"
-sh lambda-functions/create_lambda_stack.sh $PROFILE "dkhp-lambda-stack"
-sh load-balancer/create_lb_stack.sh $PROFILE "dkhp-lb-stack"
+# sh network/create_network_stack.sh $PROFILE "dkhp-network-stack"
+# sh iam/create_iam_stack.sh $PROFILE "dkhp-iam-stack"
+# sh rds/create_rds_stack.sh $PROFILE "dkhp-rds-stack"
+# sh dynamodb/create_dynamodb_stack.sh $PROFILE "dkhp-dynamodb-stack"
+# sh efs/create_efs_stack.sh $PROFILE "dkhp-efs-stack"
+# # sh lambda-functions/create_lambda_stack.sh $PROFILE "dkhp-lambda-stack"
+# sh load-balancer/create_lb_stack.sh $PROFILE "dkhp-lb-stack"
 sh ecs-task-definitions/create_ecs_tf_stack.sh $PROFILE "dkhp-ecs-tf-stack"
 sh ecs/create_ecs_stack.sh $PROFILE "dkhp-ecs-stack"
 
--- destroy.sh
+++ destroy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e 
 
 PROFILE="personal"
 
 delete_stack_if_exists() {
     local stack_name=$1
 
-    if aws cloudformation describe-stacks --stack-name "$stack_name" >/dev/null 2>&1; then
+    if aws cloudformation describe-stacks --profile "$PROFILE" --stack-name "$stack_name" >/dev/null 2>&1; then
         echo "Deleting stack: $stack_name"
-        aws cloudformation delete-stack --stack-name "$stack_name"
+        aws cloudformation delete-stack --profile "$PROFILE" --stack-name "$stack_name"
         
         aws cloudformation wait stack-delete-complete --profile "$PROFILE" --stack-name "$stack_name"
         echo "Deleted stack $stack_name successfully"
@@ -17,16 +18,16 @@ delete_stack_if_exists() {
 }
 
 # Delete stacks in order
-delete_stack_if_exists "mdms-ecs-stack"
-delete_stack_if_exists "mdms-ecs-tf-stack"
-delete_stack_if_exists "mdms-lb-stack"
-delete_stack_if_exists "mdms-lambda-stack"
-delete_stack_if_exists "mdms-ecr-stack"
-delete_stack_if_exists "mdms-efs-stack"
-delete_stack_if_exists "mdms-dynamodb-stack"
-delete_stack_if_exists "mdms-rds-stack"
-delete_stack_if_exists "mdms-iam-stack"
-delete_stack_if_exists "mdms-network-stack"
+delete_stack_if_exists "dkhp-ecs-stack"
+delete_stack_if_exists "dkhp-ecs-tf-stack"
+delete_stack_if_exists "dkhp-lb-stack"
+delete_stack_if_exists "dkhp-lambda-stack"
+delete_stack_if_exists "dkhp-ecr-stack"
+delete_stack_if_exists "dkhp-efs-stack"
+delete_stack_if_exists "dkhp-dynamodb-stack"
+delete_stack_if_exists "dkhp-rds-stack"
+delete_stack_if_exists "dkhp-iam-stack"
+delete_stack_if_exists "dkhp-network-stack"
 
 echo "Deleted all stack successfully"
 exit 0
